58dba392d018f78faf2314ee4a8c1da9a55ee449
einaur <eanorway@gmail.com>
2025-06-26 21:06:16 +0200

    rm unused file

--- grid_tdhf/setup/time_propagation.py
+++
@@ -1,5 +0,0 @@
-def setup_time_propagation(imaginary=False):
-    if imaginary:
-        from grid_tdhf.time_propagation.imag import run_imag_time_propagation
-    else:
-        from grid_tdhf.time_propagation.imag import run_time_propagation
